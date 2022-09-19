a82bde450548e4ce2a145ce38f0848301b8dae7f
Esteban Ruiz <ed.esteban.ruiz@gmail.com>
2022-09-19 14:14:26 -0500

    Test log

--- Master/InstallKubernetesMaster.sh
+++ Master/InstallKubernetesMaster.sh
@@ -3,7 +3,7 @@ echo "Change Hostname..."
 sudo hostnamectl set-hostname "k8smaster.example.net"
 echo "Done"
 exec bash
-
+echo "bash"
 echo "Edit the nodes with the information"
 echo "  192.168.1.173   k8smaster.example.net k8smaster
         192.168.1.174   k8sworker1.example.net k8sworker1
